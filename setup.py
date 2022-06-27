a5ab878ea93bcc26695602bf21b02651e47a3b07
Soni-G <>
2022-06-27 17:29:29 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description='MLW functions library',
     author='SAG',
     license='MIT',
-    install_requires=["pandas==1.1.5"],
+    install_requires=["pandas"],
     test_suite='tests',
 )
